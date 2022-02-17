32f45d53f2ab1ce764459deb3e7ce74f76f2f966
Svetlana Kutyova <kutyovas@yandex.ru>
2022-02-17 22:07:47 +0300

    work

---
+++ src/js/__tests__/luhnAlgorithm.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+// import '@testing-library/jest-dom/extend-expect';
+// import './__tests__/jest-dom/extend-expect.js';
+import luhnAlgorithm from '../luhnAlgorithm.js';
+
+describe('Функция IsValid должен работать корректно', () => {
+  test.each([
+    ['Тест 1 false', '4561 2612 1234 5464', false],
+    ['Тест 2 true', '4561 2612 1234 5467', true],
+    ['Тест 3 true', '379791071524836', true],
+    ['Тест 4 false', '2222222222222222222', false],
+    ['Тест 5', '0000000000000000', false],
+  ])(('Должен быть %s'), (_, input, expected) => {
+    expect(luhnAlgorithm(input)).toBe(expected);
+  });
+});
---
+++ src/js/__tests__/paySystem.js
@@ -0,0 +1,18 @@
+import paySystem from '../paySystem.js';
+
+describe(' на наличие в объекте paySistem ключа с таким значением', () => {
+  test.each([
+    ['Test 1', '36865410416253', 'div.col.col1'],
+    ['Test 2', '4716983987165598', 'div.col.col2'],
+    ['Test 3', '5233636706819611', 'div.col.col3'],
+    ['Test 4', '6762976812058473', 'div.col.col6'],
+    ['Test 5', '1222222222222222', ''],
+    ['Test 6', '347194578472253', 'div.col.col1'],
+    ['Test 7', '9020286690138979', ''],
+    ['Test 8', '5038800500236938', 'div.col.col6'],
+    ['Test 9', '6759394784053479', 'div.col.col6'],
+    ['Test 10', '2221003479698600', 'div.col.col6'],
+  ])(('Если в %s передаём %d должен вернуть %s'), (_, input, expected) => {
+    expect(paySystem(input)).toBe(expected);
+  });
+});
